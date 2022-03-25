10c12166f03115e03c29aa1a841dcb1656257898
mrserjo <Glorytorus1488>
2022-03-25 22:49:04 +0300

    hw

--- app.py
+++ app.py
@@ -24,9 +24,23 @@ class Article(db.Model):
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
 
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
@@ -40,7 +54,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return 'При добавлении статьи произошла ошибка'
     else:
